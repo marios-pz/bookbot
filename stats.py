834c9c91979f66a704291745a435b5ff5f0ba170
marios-pz <marios-pz@proton.me>
2025-03-03 19:21:05 +0200

    opt: improve get_chars_frequency by using Counter()

--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def get_book_text(input_file_path: str):
     with open(input_file_path) as f:
         file_contents = f.read()
@@ -9,19 +12,24 @@ def get_number_of_words(contents: str):
     return num_words
 
 
+# 'Pythonic'
 def get_chars_frequency(contents: str) -> dict[str | int]:
-    chars_frequency = {}
+    return Counter(contents.lower())
 
-    # initialize NOTE: improve this
-    for char in contents:
-        char = char.lower()
-        chars_frequency[char] = 0
 
-    for char in contents:
-        char = char.lower()
-        chars_frequency[char] += 1
-
-    return chars_frequency
+# original
+# def get_chars_frequency(contents: str) -> dict[str | int]:
+#     chars_frequency = {}
+#
+#     for char in contents:
+#         char = char.lower()
+#         chars_frequency[char] = 0
+#
+#     for char in contents:
+#         char = char.lower()
+#         chars_frequency[char] += 1
+#
+#     return chars_frequency
 
 
 def report_char_frequency(chars_frequency: dict[str | int]) -> None:
